d1b4b8328ada0aefd72099e84c1e326840d05fa9
HarshKakran <harshkakran05@gmail.com>
2020-10-06 12:15:52 +0530

    Creates an APIView

--- src/profiles_project/profile_api/views.py
+++ src/profiles_project/profile_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+class HelloApiView(APIView):
+    #Test API View
+
+    def get(request, self, format=None):
+        #Returns a list
+
+        list = ['Hello',
+                'How are you',
+                'What are you doing']
+
+        return Response({'list':list})
